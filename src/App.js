6f5de5db60131cab06c957d091b75d915000724d
Ilyas <79158764+mr-ceri-mrum@users.noreply.github.com>
2025-04-11 05:28:40 +0500

    Обновлен корневой компонент приложения

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import StartGame from './components/StartGame';
-import GameInterface from './components/GameInterface';
-import { useGameStore } from './store/gameStore';
+/**
+ * Корневой компонент приложения
+ */
+import React, { useEffect } from 'react';
+import { useGameStore } from './store';
+import StartGame from './components/game/StartGame';
+import GameInterface from './components/game/GameInterface';
 
-function App() {
-  const { gameStarted } = useGameStore();
-
+/**
+ * Основной компонент приложения
+ * Определяет, показывать стартовый экран или игровой интерфейс
+ */
+const App = () => {
+  const { gameStarted, showSuccessNotification } = useGameStore((state) => ({
+    gameStarted: state.gameStarted,
+    showSuccessNotification: state.showSuccessNotification
+  }));
+  
+  // При монтировании компонента показываем приветственное сообщение
+  useEffect(() => {
+    if (!gameStarted) {
+      showSuccessNotification('Добро пожаловать в IT Simulator!', 5000);
+    }
+  }, []);
+  
   return (
     <div className="app-container">
       {!gameStarted ? <StartGame /> : <GameInterface />}
     </div>
   );
-}
+};
 
 export default App;
